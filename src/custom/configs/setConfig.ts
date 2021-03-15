8a134180d434656ca2fcf2f787ecb4a1af869eaa
YizYah <yyakovson613@gmail.com>
2021-03-15 16:34:58 +0200

    feat(everything): created tests and refactored as needed

--- src/custom/configs/setConfig.ts
+++ src/custom/configs/setConfig.ts
@@ -7,11 +7,11 @@ const yaml = require('js-yaml')
 export async function setConfig(templateDir: string, config: Configuration) {
   const configFile = templateDir + '/' + CONFIG_FILE
   try {
-    if (config) await fs.outputFile(configFile, yaml.dump(config))
+    if (!await fs.pathExists(templateDir)) throw new Error(`no directory ${templateDir}.  Confirm your template path.`)
+
+    await fs.outputFile(configFile, yaml.dump(config))
   } catch (error) {
-    throw new Error(`error finding the config file ${configFile}.
-It may be that the template location is faulty, or that the file does not exist
-or is not properly set up:
+    throw new Error(`cannot save the config file ${configFile}:
 ${error}`)
   }
 }
